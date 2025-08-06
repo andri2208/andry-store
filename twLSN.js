3effb8ff96bc870f32e5f7bff85011b0f4672283
andri2208 <78659880+andri2208@users.noreply.github.com>
2025-08-06 21:03:14 +0700

    Update twLSN.js

--- twLSN.js
+++ twLSN.js
@@ -3,6 +3,8 @@ var twLSN = [
 'toko-whatsapp.blogspot.com',
 'jasa-design.web.id',
 'www.jasa-design.web.id',
+  'andrystore01.blogspot.com',
 ];
+
 
 var redirectUrl = 'http://bit.ly/toko-whatsapp';
